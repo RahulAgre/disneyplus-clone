492c275e6004d749c73a335d007698d64154f078
Raaya <rahulagre555@gmail.com>
2021-09-28 13:54:10 +0530

    Starting with different components

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" alt="" />
+          <span>HOME</span>
+        </a>
+      </NavMenu>
+    </Nav>
+  );
+}
+
+export default Header;
+
+const Nav = styled.div`
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+`;
+
+const Logo = styled.img`
+  width: 80px;
+`;
+const NavMenu = styled.div``;
