2cfa2031e83967569d272d87f728a5e2b65d7752
Raaya <rahulagre555@gmail.com>
2021-09-30 20:16:08 +0530

    Added content to Detail component

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,7 +1,151 @@
 import React from "react";
+import styled from "styled-components";
 
 function Detail() {
-  return <div>Detail</div>;
+  return (
+    <Container>
+      <Background>
+        <img
+          src="https://bingeddata.s3.amazonaws.com/uploads/2020/11/bao-1.jpg"
+          alt=""
+        />
+      </Background>
+
+      <ImageTitle>
+        <img src="https://imgur.com/2pKwvc1.png" alt="" />
+      </ImageTitle>
+
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" alt="" />
+          <span>PLAY</span>
+        </PlayButton>
+
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" alt="" />
+          <span>Trailer</span>
+        </TrailerButton>
+
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+
+        <GroupWatchButton>
+          <img src="/images/group-icon.png" alt="" />
+        </GroupWatchButton>
+      </Controls>
+
+      <SubTitle>2018 • 7m • Family, Fantasy, Kids, Animation</SubTitle>
+
+      <Description>
+        A Chinese mom who's sad when her grown son leaves home gets another
+        chance at motherhood when one of her dumplings springs to life. But she
+        finds that nothing stays cute and small forever.
+      </Description>
+    </Container>
+  );
 }
 
 export default Detail;
+
+const Container = styled.div`
+  min-height: calc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opacity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  min-height: 170px;
+  width: 35vw;
+  min-width: 200px;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  padding: 0px 24px;
+  margin-right: 22px;
+  display: flex;
+  align-items: center;
+  height: 56px;
+  background-color: rgb(249, 249, 249);
+  border: none;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+
+  &:hover {
+    background: rgb(198, 198, 198);
+    transition: 200ms;
+  }
+`;
+
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+`;
+
+const AddButton = styled.button`
+  margin-right: 16px;
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+
+const GroupWatchButton = styled(AddButton)`
+  background: rgb(0, 0, 0);
+`;
+
+const SubTitle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 26px;
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  color: rgb(249, 249, 249);
+`;
