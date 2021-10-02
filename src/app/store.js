175bed51f438eb899fffe92c8ace14bdf674e793
Raaya <rahulagre555@gmail.com>
2021-10-02 14:19:47 +0530

    updated necessary changes

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/counterSlice";
 import movieReducer from "../features/movie/movieSlice";
 
 export const store = configureStore({
