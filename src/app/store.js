95a426a17d92d091eb98c6576b2503d750a6ae40
Raaya <rahulagre555@gmail.com>
2021-10-03 22:29:15 +0530

    passed user slice to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import movieReducer from "../features/movie/movieSlice";
+import userReducer from "../features/user/userSlice";
+import userSlice from "../features/user/userSlice";
 
 export const store = configureStore({
   reducer: {
     movie: movieReducer,
+    user: userSlice,
   },
 });
